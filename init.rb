6c9b258db1db38bb75c388bc06f5331f98d53590
Oleg Sinizin <oleg.sinizin@onlyoffice.com>
2021-09-24 16:31:40 +0300

    add 3rd-Party.license, set plugin info in init.rb

--- init.rb
+++ init.rb
@@ -2,11 +2,11 @@ require 'redmine'
 
 Redmine::Plugin.register :onlyoffice_redmine do
   name 'Onlyoffice Redmine plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'ONLYOFFICE'
+  description 'Redmine ONLYOFFICE integration plugin'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/ONLYOFFICE/onlyoffice-redmine'
+  author_url 'https://www.onlyoffice.com'
 
 
   settings default: {'oo_address' => 'http://localhost/',
