ea55d5a6eff3b75c1246c78ff3c8c0f5e7278407
Andrey Yumatov <Andrey.yumatov@onlyoffice.com>
2022-03-23 20:31:59 +0300

    Add a js function to add a conversion button

--- assets/javascripts/onlyofficeMenu.js
+++ assets/javascripts/onlyofficeMenu.js
@@ -57,3 +57,64 @@ var addOnlyOfficeCreateButton = function(containerElement) {
         dropdown.classList.remove("hidden");
     }
 }
+
+var addOnlyOfficeConvert = function(formats, attachmentsDiskFilename, pageId, pageType) {
+    if (document.getElementsByClassName("attachments")[0] != null) {
+        var attachmentsTable = document.getElementsByClassName("attachments")[0].children[1];
+        var attachmentsList = attachmentsTable.children[0].children;
+
+        for (var i = 0; i < attachmentsList.length; i++) {
+            var ext = attachmentsDiskFilename[i].substring(attachmentsDiskFilename[i].lastIndexOf("."));
+            if (formats.indexOf(ext) !== -1) {
+                var convertButton = document.createElement("a");
+
+                convertButton.id = "onlyoffice-button-convert-" + i;
+                convertButton.className = "onlyoffice-editor-button-convert icon-only";
+                convertButton.style.backgroundImage = 'url(/plugin_assets/onlyoffice_redmine/images/conversion.svg)';
+                convertButton.style.backgroundSize = "16px";
+                convertButton.style.margin = "0 6px";
+
+                let attachmentHref = attachmentsList[i].children[0].children[0].href;
+                convertButton.href = "#";
+                convertButton.onclick = function () {
+                    window.open(window.location.origin + "/onlyoffice/conversion/"+ pageId + "/" + pageType + attachmentHref.substring(attachmentHref.lastIndexOf("/")));
+                    window.location.reload();
+                }
+                var deleteButton = attachmentsList[i].children[attachmentsList[i].children.length - 1].getElementsByClassName("delete icon-only icon-del")[0];
+                if (!!deleteButton) {
+                    attachmentsList[i].children[attachmentsList[i].children.length - 1].insertBefore(convertButton, deleteButton);
+                } else {
+                    attachmentsList[i].children[attachmentsList[i].children.length - 1].appendChild(convertButton);
+                }
+            }
+        }
+    } else if (document.getElementsByTagName("tbody")[0] != null) {
+        attachmentsList = document.getElementsByTagName("tbody")[0].children;
+        for (var i = 0; i < attachmentsList.length; i++) {
+            var filename = attachmentsDiskFilename[i].firstChild.innerText;
+            var ext = filename.substring(filename.lastIndexOf("."));
+            if (formats.indexOf(ext) !== -1) {
+                var convertButton = document.createElement("a");
+
+                convertButton.id = "onlyoffice-button-convert-" + i;
+                convertButton.className = "onlyoffice-editor-button-convert icon-only";
+                convertButton.style.backgroundImage = 'url(/plugin_assets/onlyoffice_redmine/images/conversion.svg)';
+                convertButton.style.backgroundSize = "16px";
+                convertButton.style.margin = "0 6px";
+
+                let attachmentHref = document.getElementsByClassName("icon-only icon-del")[i].href;
+                convertButton.href = "#";
+                convertButton.onclick = function () {
+                    window.open(window.location.origin + "/onlyoffice/conversion/"+ pageId + "/" + pageType + attachmentHref.substring(attachmentHref.lastIndexOf("/")));
+                    window.location.reload();
+                }
+                var downloadButton = document.getElementsByClassName("icon-only icon-download")[i];
+                if (!!downloadButton) {
+                    attachmentsList[i].children[attachmentsList[i].children.length - 1].insertBefore(convertButton, downloadButton);
+                } else {
+                    attachmentsList[i].children[attachmentsList[i].children.length - 1].appendChild(convertButton);
+                }
+            }
+        }
+    }
+}
